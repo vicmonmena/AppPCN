4b86f9abe28d90b31cbb2931f21b695aa55e3f44
Vicente <vicmonmena@gmail.com>
2015-06-09 09:32:28 +0200

    Autenticación y autorización con plantilla básica
    Autenticación y autorización con plantilla básica

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=apppcn',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
